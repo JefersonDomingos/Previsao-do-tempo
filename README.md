067a4d35a09b1a69f08d68850ffa363c9568f468
Jeferson Domingos <jefersondomingos77@gmail.com>
2022-10-02 23:53:16 -0300

    corrigindo key do projeto

---
+++ README.md
@@ -0,0 +1,35 @@
+# <h1 align="center">  App de consulta climática </h1>
+
+## <b>Sobre o projeto</b> </br>
+Este projeto foi criado para que o usuário possa consultar as condições climáticas em diversas cidades do mundo, é um projeto que usa uma API extraida do site:
+https://openweathermap.org/api e que fornece dados como por exemplo: velocidade dos ventos, umidade do ar, além de trazer a bandeira de cada país que a cidade buscada pertence.
+
+## Veja o video
+https://user-images.githubusercontent.com/105239532/193367800-3d3d05a9-0001-41f2-809b-76adc365a2ba.mp4
+
+
+## Tecnologias
+
+- HTML5</br>
+- CSS3</br>
+- JavaScript
+- Weather API
+
+## Imagens
+
+![/Projeto concluido.jpg"](https://github.com/JefersonDomingos/Consulta-Climatica/blob/5ba0771a4ba83b50a1062c830d49cb78da0813f2/assets/IMG/Projeto%20concluido.jpg)
+
+
+## Para rodar o projeto
+
+basta apenas abrir um navegador de sua preferência e acessar o link abaixo:
+
+## Link
+
+https://consulta-climatica.vercel.app/
+
+## Autor
+Jeferson Domingos Martins
+
+- [LinkedIn](https://www.linkedin.com/in/jefersondomingos)</br>
+- [GitHub](https://github.com/JefersonDomingos)
---
+++ js/scripts.js
@@ -0,0 +1,103 @@
+//Seleção de elementos
+
+const apiCountryUrl = "https://countryflagsapi.com/png/";
+
+const inputCity = document.querySelector("#city-input");
+const btnSearch = document.querySelector("#search");
+
+const elementCity = document.querySelector("#city");
+const elementCountry = document.querySelector("#country");
+const elementTemp = document.querySelector("#temperature span");
+const elementDesc = document.querySelector("#description");
+const elementImgIcon = document.querySelector("#weather-icon");
+const elementHumidity = document.querySelector("#humidity span");
+const elementWind = document.querySelector("#wind span");
+const elementWeatherData = document.querySelector("#weather-data");
+const elementMessageError = document.querySelector("#error-mesage");
+const elementDivergenceMessage = document.querySelector("#divergence-mesage");
+const elementInstruction = document.querySelector("#instruction");
+
+
+//Funções
+const getWeatherData = async (city) => {
+    try{
+            const apiWeatherUrl = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${keyApi}&lang=pt_br`);
+            const data = await apiWeatherUrl.json();
+            
+            if(data.erro){
+                    throw Error();
+            }
+
+            elementCity.innerText = data.name;
+            elementTemp.innerText = parseInt(data.main.temp);
+            elementDesc.innerText = data.weather[0].description;
+            elementImgIcon.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+            elementCountry.setAttribute("src", apiCountryUrl + data.sys.country);
+            elementHumidity.innerText = `${data.main.humidity}%`;
+            elementWind.innerText = `${data.wind.speed}km/h`;
+            elementMessageError.classList.add("hide");
+            elementWeatherData.classList.remove("hide");
+            elementDivergenceMessage.classList.remove("hide");
+
+            console.log(data);
+            
+            return data;
+
+        } 
+
+        //Erros api
+
+        catch(erro){
+            otherMistakes();
+        }
+}
+
+const otherMistakes = () =>{
+    elementWeatherData.classList.add("hide");
+    elementMessageError.innerText = "o nome da cidade está incorreto";
+    elementMessageError.classList.remove("hide");
+    elementDivergenceMessage.classList.add("hide");
+    
+}
+
+const inputEmpyt = () =>{
+    elementWeatherData.classList.add("hide");
+    elementMessageError.innerText = " o nome não pode ficar vazio";
+    elementMessageError.classList.remove("hide");
+    elementDivergenceMessage.classList.add("hide");
+
+}
+
+//Eventos
+btnSearch.addEventListener("click", (e) => {
+    e.preventDefault();
+    const city = inputCity.value;
+    if(city === ""){
+        inputEmpyt();
+    }
+    else{
+    getWeatherData(city);
+    }
+    
+});
+
+inputCity.addEventListener("keyup",(e) => {
+    if(e.code === "Enter"){
+        const city = e.target.value;
+        if(city === ""){
+            inputEmpyt();
+        }
+        else{
+        getWeatherData(city);
+        }
+    }
+});
+
+elementDivergenceMessage.addEventListener("click", () => {
+    elementInstruction.classList.toggle("hide");
+    console.log("instruçãoes");
+});
+
+
+
+
